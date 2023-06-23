9e2a38b40726a82e2eeabd578aa026d917fb0eea
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-23 12:41:34 +0530

    Practical 4 v2

---
+++ practical_4/WhileLoop.java
@@ -0,0 +1,16 @@
+package practical_4;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+		int number = 1;
+		
+		while(number <= 10)
+		{
+			System.out.println(number);
+			++number;
+		}
+	}
+	
+}
