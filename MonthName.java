3edd2001f7b2725cea34c5ef00c96108c0e88608
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-23 12:32:51 +0530

    Practical 3 v2

---
+++ MonthName.java
@@ -0,0 +1,61 @@
+package practical_3;
+import java.util.Scanner;
+
+public class MonthName {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Month Number: ");
+		int month = sc.nextInt();
+
+		/*
+		  Finding the current month name using switch case. 
+		 */
+		
+		switch(month)
+		{
+			case 1:
+				System.out.println("January");
+				break;
+			case 2:
+				System.out.println("Fabruary");
+				break;
+			case 3:
+				System.out.println("March");
+				break;
+			case 4:
+				System.out.println("April");
+				break;
+			case 5:
+				System.out.println("May");
+				break;
+			case 6:
+				System.out.println("june");
+				break;
+			case 7:
+				System.out.println("July");
+				break;
+			case 8:
+				System.out.println("August");
+				break;
+			case 9:
+				System.out.println("Spetember");
+				break;
+			case 10:
+				System.out.println("Octomber");
+				break;
+			case 11:
+				System.out.println("November");
+				break;
+			case 12:
+				System.out.println("December");
+				break;
+			default:
+				System.out.println("Invalid Number");
+		}
+		sc.close();
+	}
+	
+}
