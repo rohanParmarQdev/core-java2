ed2633ce807f66a18d1ba3a7f330a854b5a6ceea
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-23 12:26:07 +0530

    practical 1 v2

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package practical_1;
+
+public class HelloWorld {
+
+	/*
+	 This is the Hello World Program
+	*/
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+	}
+}
