286753fb3089c4a9796a13c637345496253a3203
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-23 15:17:58 +0530

    Practical 10 v1

---
+++ StringMethods.java
@@ -0,0 +1,18 @@
+package practical_10;
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		
+		String name1 = "Chankit";
+		String name2 = "Sharma";
+		
+		/*
+		   In this program we are using String Methods.
+		*/
+		System.out.println("Compare two Strings: " + name1.equals(name2));
+		System.out.println("Find Charctor in String: " + name1.charAt(5));
+		System.out.println("Concating two String: " + name1.concat(name2));
+	}
+	
+}
