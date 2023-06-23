570010f6152dded3a851196b5bd639f8e144203b
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-23 11:09:14 +0530

    Practical 17 v2

--- Cat.java
+++ Cat.java
@@ -2,6 +2,9 @@ package Practical_17;
 
 public class Cat implements Animal{
 
+	/*
+	This is Implemented class of Animal
+	*/
 	public void eat() {
 		System.out.println("The Cat is Eating...");
 	}
