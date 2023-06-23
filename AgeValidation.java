6ab34daee255b15a1c33a5019baa6774775d6f78
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-23 12:26:28 +0530

    Practical 2 v2

---
+++ AgeValidation.java
@@ -0,0 +1,18 @@
+package practical_2;
+
+public class AgeValidation {
+
+	public static void main(String[] args) {
+		
+		int age = 17;
+		/*
+		  Validating age group by using if else statement
+		 */
+		if (age < 18) {
+			System.out.println("You are under 18");
+		}
+		else {
+			System.out.println("You are adult");
+		}
+	}
+}
